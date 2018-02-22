ad3a8b224fbdc353275118b530111acc1e46c031
mhonlue <honlue@gmail.com>
2018-02-23 02:40:53 +0400

    Update main.py

--- marvin/main.py
+++ marvin/main.py
@@ -5,8 +5,16 @@ import json
 import subprocess, socket
 from _utils import ping_options, runPing, punyHostname
 from _parsePing import marvinPingParser
+from marvin_config import *
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=[limitValue()],
+)
 
 
 puppeteer = 'http://localhost:3001';
@@ -16,35 +24,13 @@ def root():
 	return('Welcome')
 
 @app.route('/info')
+@limiter.exempt
 def getInfo(): # This shouldn't be static anymore
-	response = {
-				  "type": "Chrome Marvin",
-				  "version": "1.0.0",
-				  "browser": {
-					"name": "Chromium",
-					"version": "62.0.3170.2"
-				  },
-				  "instance_type": "nat64",
-				  "name": "sjms-42",
-				  "network": {
-					"ipv4": {
-					  "address": "192.168.0.42/24",
-					  "gateway": "192.168.0.1"
-					},
-					"ipv6": {
-					  "address": "2001:db8::42/64",
-					  "gateway": "2001:db8::1"
-					},
-					"dns_server": "2001:db8::53"
-				  },
-				  "limits": {
-					"parallel_tasks": 15
-				  }
-				}
-	return jsonify(response), 200
+	return(infoValues()), 200
 
 @app.route('/ping/', methods = ["GET","POST"])
 @app.route('/ping/<hostname>', methods = ["GET","POST"])
+#@limiter.limit(limitValue())
 def ping(hostname):
 	if request.method == 'POST':
 		data = ping_options()
@@ -56,7 +42,7 @@ def ping(hostname):
 		
 		result = marvinPingParser(hostname, data, output)
 		
-		pprint(result)
+		pprint(output)
 		
 		return(json.dumps(result, indent=4)),200
 		
@@ -65,21 +51,45 @@ def ping(hostname):
 
 @app.route('/ping6/', methods = ["GET","POST"])
 @app.route('/ping6/<hostname>', methods = ["GET","POST"])
+#@limiter.limit(limitValue())
 def ping6(hostname):
 	if request.method == 'POST':
-		request.get_json(force=True);
-		# request.data ~= {"size":"56","count":"5","pmtu":"want","timeout":"60"}
-		dataBytes = request.data
-		data = json.loads(dataBytes) #Convert from bytes format to JSON
+		data = ping_options()
 
 		cmd = '-c%s -n -s%s -M%s'% (data['count'] , data['size'] , data['pmtu'])
-		parser.parse(runPing(['ping6', cmd , hostname]))
+		output = runPing(['ping', cmd , punyHostname(hostname)])
+		
+		result = marvinPingParser(hostname, data, output)
+		
+		pprint(output)
 
 		pprint('ping6 %s %s' %(cmd, hostname))
-		return(json.dumps(parser.as_dict(), indent=4)),200
+		
+		return(json.dumps(result, indent=4)),200
 	else:
 		return('Method not allowed!'), 400
 
+@app.route('/request/', methods = ["GET","POST"])
+@app.route('/request/<url>', methods = ["GET","POST"])
+def screenAndRequest():
+	if request.method == 'POST':
+		if not is_empty(request.data):
+			request.get_json(force=False);
+		# request.data ~= {"viewport":[1024,1024],"timeout":60}
+		# FIXME - Parse the data more securely and efficiently - type &| length verification
+		if 'timeout' not in request.json:
+			request.json['timeout'] = 60
+		if 'viewport' not in request.json:
+			request.json['viewport'] = [1024,1024]
+		
+		pprint(request.json)
+		
+		payload = {'url': 'https://google.com', 'viewport': request.json['viewport'], 'timeout': request.json['timeout']}
+		pprint(payload)
+		output = requests.post(puppeteer + '/request', json = payload).content
+		return(output),200
+	else:
+		return('Method not allowed!'), 400
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=3000, debug=True)
